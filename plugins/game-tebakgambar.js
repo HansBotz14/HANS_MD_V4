1421c273b8ced511f3465ec0f22e816170c058f3
HansBotz14 <117080209+HansBotz14@users.noreply.github.com>
2022-11-20 07:24:41 +0700

    Update game-tebakgambar.js

--- plugins/game-tebakgambar.js
+++ plugins/game-tebakgambar.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 500
+let poin = 50000
 let src
 let handler = async (m, { conn, usedPrefix }) => {
   conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
